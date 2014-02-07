aa2cc5cf5b7d8756c7247a80719c083e0f61c996
roysbailey <roysbailey@silvertouch.co.uk>
2014-02-07 08:44:24 +0000

    Added jade and bootstrap header

---
+++ CLMUI/routes/home.js
@@ -0,0 +1,7 @@
+/**
+ * Created by rbailey on 07/02/14.
+ */
+
+exports.index = function(req, res){
+    res.render('home/index');
+}
--- CLMUI/server.js
+++ CLMUI/server.js
@@ -1,6 +1,18 @@
-var http = require('http');
-var port = process.env.port || 1337;
-http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port);
+var express = require('express'),
+    path = require('path'),
+    home = require('./routes/home.js'),
+    port = process.env.port || 1337;
+
+var app = express();
+
+app.configure(function () {
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'jade');
+    app.use(express.bodyParser({ keepExtensions: true, uploadDir: path.join(__dirname, '/pictures')}));
+    app.use(express.methodOverride());
+    app.use(express.static(path.join(__dirname, 'public')));
+});
+
+app.get('/', home.index);
+
+app.listen(port);
